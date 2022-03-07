b8d532b5197e5bb8b00c0eedfdc4750b40c21077
luuhoa0801 <hoathuytien12369@gmail.com>
2022-03-07 15:39:13 +0700

    update

---
+++ MODUL1/ss7_bien_kieu_du_lieu_va_toan_tu/thuc_hanh/su_dung_toan_tu/jscript/scripttoantu.js
@@ -0,0 +1,14 @@
+
+let inputWidth;
+let inputHeight;
+
+inputWidth = prompt("Enter the width");
+inputHeight = prompt("Enter the hight");
+
+let width = parseInt(10);
+let hight = parseInt(20);
+
+
+
+let area = width * hight ;
+document.write('the area is '+ area);
